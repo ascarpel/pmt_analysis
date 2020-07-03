cd0429819babd6c785fb4e3ce033c107c00fc2ff
Andrea Scarpelli <ascarpell@dune0001.sdcc.bnl.gov>
2020-07-03 17:01:54 -0400

    Add some examples files for BNL SDCC

---
+++ darkrate/processPulses.sh
@@ -0,0 +1,2 @@
+makePulseHist /gpfs01/lbne/users/pmt/pulses/run1717_pulses.root run1717_nocuts_darkrates.root -1 0
+makePulseHist /gpfs01/lbne/users/pmt/pulses/run1717_pulses.root run1717_darkrates.root -1 1
